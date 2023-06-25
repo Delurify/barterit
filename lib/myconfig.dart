dab67b58bfde97539b1cbc6681f76156ada83310
asus <atphng@hotmail.com>
2023-06-25 14:53:33 +0800

    search item done

--- lib/myconfig.dart
+++ lib/myconfig.dart
@@ -2,10 +2,10 @@ class MyConfig {
   // String SERVER = "http://10.19.40.113";
 
   // Phone data ip address
-  String SERVER = "http://192.168.35.250";
+  // String SERVER = "http://192.168.35.250";
 
   //Maybank wifi ip address
-  // String SERVER = "http://10.19.117.168";
+  String SERVER = "http://10.19.117.168";
 
   // Home wifi address
   // String SERVER = "http://192.168.0.175";
--- lib/views/screens/searchscreen.dart
+++ lib/views/screens/searchscreen.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
 import '../../models/item.dart';
 import '../../models/user.dart';
 import '../../myconfig.dart';
+import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
+
+import 'itemdetailscreen.dart';
 
 class SearchScreen extends StatefulWidget {
   final User user;
@@ -27,9 +32,12 @@ class _SearchScreenState extends State<SearchScreen>
   List<Item> newItems = <Item>[];
   int offset = 0;
   int limit = 10;
+  late int axiscount;
   bool firstLoad = true;
   bool isLoading = false;
   bool hasMore = true;
+  final df = DateFormat('d MMM');
+  final controller = ScrollController();
 
   final TextEditingController _searchEditingController =
       TextEditingController();
@@ -43,49 +51,241 @@ class _SearchScreenState extends State<SearchScreen>
   }
 
   @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     bool isDark = Theme.of(context).brightness == Brightness.dark;
 
+    if (screenWidth > 600) {
+      axiscount = 3;
+    } else {
+      axiscount = 2;
+    }
+
     return Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          floating: true,
-          pinned: true,
-          iconTheme: const IconThemeData(color: Colors.white),
-          title: Container(
-            decoration: BoxDecoration(
-                color: isDark ? Colors.grey[800] : Colors.white,
-                borderRadius: const BorderRadius.all(Radius.circular(20))),
-            width: double.infinity,
-            height: 40,
-            child: Center(
-              child: TextField(
-                controller: _searchEditingController,
-                decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Search for something',
-                    prefixIcon: Icon(Icons.search)),
-                onSubmitted: (v) {
-                  print("Hello");
-                },
-              ),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: Container(
+          decoration: BoxDecoration(
+              color: isDark ? Colors.grey[800] : Colors.white,
+              borderRadius: const BorderRadius.all(Radius.circular(20))),
+          width: double.infinity,
+          height: 40,
+          child: Center(
+            child: TextField(
+              controller: _searchEditingController,
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search for something',
+                  prefixIcon: Icon(Icons.search)),
+              onSubmitted: (v) {
+                setState(() {
+                  isLoading = false;
+                  hasMore = true;
+                  offset = 0;
+                  itemList.clear();
+                });
+                searchitems(v);
+              },
             ),
           ),
-          bottom: TabBar(
-              controller: _tabController,
-              labelColor: Colors.white,
-              tabs: const [
-                Tab(text: 'Items'),
-                Tab(
-                  text: 'People',
-                )
-              ]),
+        ),
+        bottom: TabBar(
+            controller: _tabController,
+            labelColor: Colors.white,
+            tabs: const [
+              Tab(text: 'Items'),
+              Tab(
+                text: 'People',
+              )
+            ]),
+      ),
+      body: TabBarView(controller: _tabController, children: [
+        Center(
+            child: Column(
+          children: [
+            Expanded(
+                child: RefreshIndicator(
+              onRefresh: refresh,
+              child: GridView.count(
+                  childAspectRatio: 4 / 5,
+                  crossAxisCount: axiscount,
+                  controller: controller,
+                  children: List.generate(
+                    itemList.length + 1,
+                    (index) {
+                      if (index < itemList.length) {
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          clipBehavior: Clip.antiAlias,
+                          elevation: 2,
+                          child: InkWell(
+                            onTap: () async {
+                              Item singleitem =
+                                  Item.fromJson(itemList[index].toJson());
+                              await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (content) => ItemDetailScreen(
+                                          user: widget.user,
+                                          useritem: singleitem,
+                                          page: "user"))).then((value) {
+                                itemList[index] = value;
+                                searchitems(_searchEditingController.text);
+                              });
+                            },
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Expanded(
+                                      child: itemList[index].itemImageCount ==
+                                              "1"
+                                          ? CachedNetworkImage(
+                                              width: screenWidth,
+                                              fit: BoxFit.cover,
+                                              imageUrl:
+                                                  "${MyConfig().SERVER}/barterit/assets/items/${itemList[index].itemId}-1.png",
+                                              placeholder: (context, url) =>
+                                                  const LinearProgressIndicator(),
+                                              errorWidget:
+                                                  (context, url, error) =>
+                                                      const Icon(Icons.error),
+                                            )
+                                          : itemList[index].itemImageCount ==
+                                                  "2"
+                                              ? ImageSlideshow(
+                                                  width: screenWidth,
+                                                  initialPage: 0,
+                                                  children: [
+                                                      Image.network(
+                                                        "${MyConfig().SERVER}/barterit/assets/items/${itemList[index].itemId}-1.png",
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                                      Image.network(
+                                                        "${MyConfig().SERVER}/barterit/assets/items/${itemList[index].itemId}-2.png",
+                                                        fit: BoxFit.cover,
+                                                      )
+                                                    ])
+                                              : ImageSlideshow(
+                                                  width: screenWidth,
+                                                  initialPage: 0,
+                                                  children: [
+                                                      Image.network(
+                                                        "${MyConfig().SERVER}/barterit/assets/items/${itemList[index].itemId}-1.png",
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                                      Image.network(
+                                                        "${MyConfig().SERVER}/barterit/assets/items/${itemList[index].itemId}-2.png",
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                                      Image.network(
+                                                        "${MyConfig().SERVER}/barterit/assets/items/${itemList[index].itemId}-3.png",
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                                    ])),
+                                  Row(
+                                    children: [
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                      Expanded(
+                                        child: SingleChildScrollView(
+                                          // for horizontal scrolling
+                                          scrollDirection: Axis.horizontal,
 
-          
-        )
-      ],
-    ));
+                                          child: Text(
+                                            itemList[index].itemName.toString(),
+                                            style:
+                                                const TextStyle(fontSize: 18),
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      const SizedBox(
+                                        width: 4,
+                                      ),
+                                      Column(
+                                        children: [
+                                          SizedBox(
+                                            width: screenWidth * 0.25,
+                                            child: Text(
+                                              "${itemList[index].itemLocality} - ${itemList[index].itemState}",
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.grey),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: screenWidth * 0.25,
+                                            child: Text(
+                                              df.format(DateTime.parse(
+                                                  itemList[index]
+                                                      .itemDate
+                                                      .toString())),
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.orange),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Container(
+                                        width: screenWidth * 0.2,
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(
+                                              10.0), // Adjust the border radius as needed
+                                          color: isDark
+                                              ? Colors.black54
+                                              : Colors.grey[
+                                                  300], // Background color of the rounded rectangle
+                                        ),
+                                        padding: const EdgeInsets.all(
+                                            5.0), // Adjust the padding as needed
+                                        child: ClipRRect(
+                                          borderRadius: BorderRadius.circular(
+                                              10.0), // Same border radius as the container
+                                          child: Text(
+                                            'RM ${double.parse(itemList[index].itemPrice.toString())}',
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ]),
+                          ),
+                        );
+                      } else {
+                        return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 32),
+                            child: Center(
+                                child: hasMore
+                                    ? const CircularProgressIndicator()
+                                    : null));
+                      }
+                    },
+                  )),
+            )),
+          ],
+        )),
+        Center()
+      ]),
+    );
   }
 
   void searchitems(String search) {
@@ -125,5 +325,16 @@ class _SearchScreenState extends State<SearchScreen>
         }
       });
     });
+  }
+
+  Future refresh() async {
+    setState(() {
+      isLoading = false;
+      hasMore = true;
+      offset = 0;
+      itemList.clear();
+    });
+
+    searchitems(_searchEditingController.text);
   }
 }
